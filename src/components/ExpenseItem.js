fa29a90979ad5bb426add126cdbc6fd678d6bc08
Gaurav Singh <gtx25801@gmail.com>
2023-03-11 13:58:03 +0530

    task 7

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h3>Expense items.</h3>
+      <ul>
+        <li>Food Rs 10.</li>
+        <li>Fuel Rs 100.</li>
+        <li>Movie Rs 500.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ExpenseItem;
